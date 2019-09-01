9f91a0502a2fad6da688eaa27c31c5ecd67ec685
Nicholas Kang <38155546+AstrodynamicistKang@users.noreply.github.com>
2019-09-01 15:50:51 +0800

    Added GPL License into program code

--- Collatz Sequence Practice.py
+++ Collatz Sequence Practice.py
@@ -1,5 +1,22 @@
 #Practise! (Collatz Sequence Exercise)
 
+print('''This is a Python program answer for the program project of Chapter 3 of
+    Automate The Boring Stuff with Python book\n''')
+print('''
+    Copyright (C) 2019 Nicholas Kang
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\n''')
+print("Feel free to contact the author at zt.kang@gmail.com or PM 'Nicholas Kang' in Orbiter Forum for bug reports and/or comments.\n")
+
 import sys
 
 def collatz(number):
